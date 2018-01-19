7eadf74a71a3e3176395b8f948a41aac55dfafaa
Aiden Yi <huichanyi@gmail.com>
2018-01-18 22:49:12 -0800

    Added testing for accounts

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -3,6 +3,22 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
+let accounts;
+
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts()
+
+  // Use of of those accounts to deploy
+  // the contract
+});
+
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    console.log(accounts);
+  });
+})
+
 /**
 how to use mocha
 
